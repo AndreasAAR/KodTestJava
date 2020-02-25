d107a59f6a264177000726fc6fbdc9c8b71f6f8a
Andreas <Andreas.ahrlund@gmail.com>
2020-02-25 10:40:12 +0100

    Has fixed the HashMap<String, HashMap<String, Relationship>>  for existing rules, implementing 'full rule' functions

--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -11,6 +11,7 @@ Figures out the
 
 public class UnitConverter {
 
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -31,10 +32,9 @@ public class UnitConverter {
 
     }
 
-
     private static void convert(String leadingUnit) {
 
-        HashMap<String,HashMap<String,Relationship>> rules = new HashMap<>();
+        HashMap<String, HashMap<String, Relationship>> rules = new HashMap<>();
         String[] unitOrder = null;
         int numLines = Integer.parseInt(leadingUnit);
         for (int i = 0; i < numLines; i++) {
@@ -46,46 +46,73 @@ public class UnitConverter {
             }
         }
         ArrayList<String> units = new ArrayList<>();
+        units.addAll(Arrays.asList(unitOrder));
 
-        List<String> list = Arrays.asList(unitOrder);
-        units.addAll(list);
-        createSystem(units, rules);
+       createSystem(units, rules);
     }
 
-    private static void addRelationship(HashMap<String,HashMap<String,Relationship>>  rules) {
-        String current = scanner.nextLine();
-        String[] lineTokens = current.split(" ");
+    //Adds relationships properly
+    private static void addRelationship(HashMap<String, HashMap<String, Relationship>> rules) {
+        String currentLine = scanner.nextLine();
+        String[] lineTokens = currentLine.split(" ");
         Relationship currentRelationship = new Relationship(lineTokens[0], Integer.parseInt((lineTokens[2])), lineTokens[3]);
 
-        if(rules.get(currentRelationship.firstUnit) == null){
-            HashMap<String,Relationship> currentMap = new HashMap<>();
-            currentMap.put(currentRelationship.secondUnit,currentRelationship);
+        //HashMap of relationships for first Unit
+       HashMap<String,Relationship> currentMap = rules.get(currentRelationship.firstUnit);
+        if (currentMap == null) {
+            currentMap = new HashMap<>();
+            currentMap.put(currentRelationship.secondUnit, currentRelationship);
+            //Put new HashMap into first Units list of relationships
             rules.put(currentRelationship.firstUnit,currentMap);
-        }else{
-           // if(rules.get(currentRelationship.firstUnit).get())
+        } else {
+            //Access existing map, add rule
+            HashMap<String, Relationship>  existingMap = rules.get(currentRelationship.firstUnit);
+            existingMap.put(currentRelationship.secondUnit,currentRelationship);
         }
     }
 
-    private static HashSet<Relationship> createSystem(ArrayList<String> units, ArrayList<Relationship> relationships) {
-        HashSet<Relationship> fullSystem = new HashSet<>();
-        fullSystem.addAll(relationships);
 
-        //Add all possible relationships
-        for(String unit : units){
+    //Makes sure ALL rules are represented!
+    private static void createSystem(ArrayList<String> units, HashMap<String, HashMap<String, Relationship>> rules) {
+        HashMap<String, HashMap<String, Relationship>> fullSystem = new HashMap<>();
 
-            //Check all existing relationships
-            for(Relationship relation: relationships){
-                if(relation.firstUnit.equals(unit)){
-
+        for(String currentUnit: units){
+            //Get every unit
+            HashMap<String,Relationship> currentSubMap = rules.get(currentUnit);
+            //If already represented
+            if(currentSubMap != null){
+                //If not all relations represented!
+                if( currentSubMap.entrySet().size() < units.size()-1 ){
+                    fillUnitRules(units,currentUnit,rules);
                 }
             }
+
         }
-        return fullSystem;
     }
+
+    private static void fillUnitRules(ArrayList<String> units, String currentUnit, HashMap<String, HashMap<String, Relationship>> rules){
+
+
+
+    }
+
+    private Relationship convertRelationship(Relationship rel1, Relationship rel2){
+        Relationship commonRelationship = null;
+
+        return commonRelationship;
+    }
+
 }
+
+
+
 
 class Relationship{
     String firstUnit;
+
+    public boolean firstSmallRelationship(){
+        return  valueInSecond < 1;
+    }
 
     public String getFirstUnit() {
         return firstUnit;
